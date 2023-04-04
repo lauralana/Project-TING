3034e53b5765a6a2418dfb08ad2bd832b40fba7c
Laura Lana <lauramarqueslana@gmail.com>
2023-04-04 19:45:38 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,20 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
 
+
+def process(path_file, instance):
+    for i in range(0, len(instance)):
+        if path_file == instance.search(i)["nome_do_arquivo"]:
+            return None
+
+    dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file)
+    }
+
+    instance.enqueue(dict)
+    sys.stdout.write(str(dict))
 
 def remove(instance):
     """Aqui irá sua implementação"""
