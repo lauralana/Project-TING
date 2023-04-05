cde14f8253170ffde35bc3ea8951bd66656b2564
Laura Lana <lauramarqueslana@gmail.com>
2023-04-04 21:14:34 -0300

    req 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -12,7 +12,7 @@ def exists_word(word, instance):
                 list['ocorrencias'].append({"linha": i + 1})
 
         print(len(list['ocorrencias']))
-        
+
         result = []
         if len(list['ocorrencias']) > 0:
             result.append(list)
@@ -21,4 +21,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    for w in range(0, len(instance)):
+        file = instance.search(w)
+        list = {
+            "palavra": word,
+            "arquivo": file['nome_do_arquivo'],
+            "ocorrencias": []
+        }
+        for i in range(0, len(file['linhas_do_arquivo'])):
+            row = file['linhas_do_arquivo'][i]
+            if word.lower() in row.lower():
+                list['ocorrencias'].append(
+                    {"linha": i + 1, "conteudo": row}
+                )
+
+        print(len(list['ocorrencias']))
+
+        result = []
+        if len(list['ocorrencias']) > 0:
+            result.append(list)
+
+    return result
